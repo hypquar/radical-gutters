0b309feb0f04413c79c5f773914a2c8df8241def
BAG33710 <46358151+BAG33710@users.noreply.github.com>
2025-11-04 10:11:11 +0500

    Merged EnemyAi with PlayerController (#11)
    Совместил скрипты EnemyAI и PlayerController - теперь враг знает о нужных состояниях игрока.

--- Assets/Core/Scripts/Enemies/PlayerStatus.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-
-[RequireComponent(typeof(CharacterController))]
-public class PlayerStatus : MonoBehaviour
-{
-    public bool IsMoving = false;
-    public bool IsCrouching = false;
-}
